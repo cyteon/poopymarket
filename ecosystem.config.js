334c8efbe5e69f645350b489087463ae4d311014
cyteon <me@cyteon.dev>
2026-06-14 15:02:52 +0200

    for pm

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+export default {
+  apps: [
+    {
+      name: "poopymarket",
+      script: ".output/server/index.mjs",
+      interpreter: "bun",
+
+      args: "--env-file=.env",
+
+      env: {
+        PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
+        NODE_ENV: "production",
+        PORT: 8008,
+      },
+    },
+  ],
+};
