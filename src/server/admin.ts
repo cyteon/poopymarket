4199f236240e83dd67e6ea02e87c6dfb1bb60e87
Cyteon <me@cyteon.dev>
2026-06-15 14:17:04 +0200

    more admin stuff

--- src/server/admin.ts
+++ src/server/admin.ts
@@ -3,8 +3,22 @@
 import { sum } from "drizzle-orm";
 import { db } from "./db";
 import { markets, trades, users } from "./db/schema";
+import { getCookie } from "vinxi/http";
+import { getUserFromToken } from "./auth";
 
 export async function getOverview() {
+  const token = getCookie("token");
+
+  if (!token) {
+    throw new Error("Unauthorized");
+  }
+
+  const user = await getUserFromToken(token);
+
+  if (!user || !user.admin) {
+    throw new Error("Unauthorized");
+  }
+
   const [totalVolume] = await db
     .select({ totalVolume: sum(trades.price).mapWith(Number) })
     .from(trades);
@@ -19,4 +33,31 @@ export async function getOverview() {
     userCount: userCount || 0,
     marketCount: marketCount || 0,
   };
+}
+
+export async function getUsers() {
+  const token = getCookie("token");
+
+  if (!token) {
+    throw new Error("Unauthorized");
+  }
+
+  const user = await getUserFromToken(token);
+
+  if (!user || !user.admin) {
+    throw new Error("Unauthorized");
+  }
+
+  const usersData = await db
+    .select({
+      id: users.id,
+      username: users.username,
+      email: users.email,
+      balance: users.balance,
+      admin: users.admin,
+      banned: users.banned,
+    })
+    .from(users);
+
+  return usersData;
 }
